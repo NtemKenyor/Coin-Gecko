9cf11bc20bb144f4778527ca25af531ec7930b91
N. K. K <60122117+NtemKenyor@users.noreply.github.com>
2021-09-15 22:47:38 +0100

    First upload
    Using the CoinGecko API to list out digital assets.

---
+++ android/app/src/main/kotlin/com/example/coin_gecko_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.coin_gecko_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_money_formatter/FlutterMoneyFormatterPlugin.h>)
+#import <flutter_money_formatter/FlutterMoneyFormatterPlugin.h>
+#else
+@import flutter_money_formatter;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterMoneyFormatterPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterMoneyFormatterPlugin"]];
+}
+
+@end
